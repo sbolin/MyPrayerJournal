f2ba957b660181266cd5cc9288b2a8eae700d06d
Scott Bolin <sbolin@gmail.com>
2021-09-06 17:38:52 +0700

    Initial Commit

---
+++ Shared/MyPrayerListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPrayerListApp.swift
+//  Shared
+//
+//  Created by Scott Bolin on 6-Sep-21.
+//
+
+import SwiftUI
+
+@main
+struct MyPrayerListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
